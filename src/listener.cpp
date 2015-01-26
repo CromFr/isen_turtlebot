f7a10c5ca3779163d7083ee22731265ad5ca0cfb
Gaetan <gaetan.lamour@isen-bretagne.fr>
2015-01-26 17:28:37 +0100

    Update ButtonEvent

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,26 +1,92 @@
 #include <iostream>
+
 using namespace std;
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <kobuki_msgs/BumperEvent.h>
+#include <kobuki_msgs/ButtonEvent.h>
 #include <kobuki_msgs/CliffEvent.h>
 
-void chatterCallback(const kobuki_msgs::BumperEventConstPtr msg)
-{
-  if(msg->state == kobuki_msgs::BumperEvent::PRESSED){
- 	ROS_INFO("PRESSED");
-  }
-  else{
-	ROS_INFO("RELEASED");
-  }
+void bumperCallback(const kobuki_msgs::BumperEventConstPtr msg){
+	if(msg->state == kobuki_msgs::BumperEvent::LEFT){
+		if(msg->state == kobuki_msgs::BumperEvent::RELEASED){
+			ROS_INFO("BUMPER LEFT RELEASED");
+		}
+		else if(msg->state == kobuki_msgs::BumperEvent::PRESSED){
+			ROS_INFO("BUMPER LEFT PRESSED");
+		}
+	}
+	else if(msg->state == kobuki_msgs::BumperEvent::CENTER){
+		if(msg->state == kobuki_msgs::BumperEvent::RELEASED){
+			ROS_INFO("BUMPER CENTER RELEASED");
+		}
+		else if(msg->state == kobuki_msgs::BumperEvent::PRESSED){
+			ROS_INFO("BUMPER CENTER PRESSED");
+		}
+	}
+	else if(msg->state == kobuki_msgs::BumperEvent::RIGHT){
+		if(msg->state == kobuki_msgs::BumperEvent::RELEASED){
+			ROS_INFO("BUMPER RIGHT RELEASED");
+		}
+		else if(msg->state == kobuki_msgs::BumperEvent::PRESSED){
+			ROS_INFO("BUMPER RIGHT PRESSED");
+		}
+	}
+}
 
-  if(msg->state == kobuki_msgs::CliffEventConstPtr msg){
-	ROS_INFO("CLIFF");
-  }
-  else{
-	ROS_INFO("FLOOR");
-  }
+void buttonCallback(const kobuki_msgs::ButtonEventConstPtr msg){
+  	if(msg->state == kobuki_msgs::ButtonEvent::Button0){
+		if(msg->state == kobuki_msgs::ButtonEvent::RELEASED){
+			ROS_INFO("BUTTON0 RELEASED");
+		}
+		else if(msg->state == kobuki_msgs::ButtonEvent::PRESSED){
+			ROS_INFO("BUTTON0 PRESSED");
+		}
+	}
+	else if(msg->state == kobuki_msgs::ButtonEvent::Button1){
+		if(msg->state == kobuki_msgs::ButtonEvent::RELEASED){
+			ROS_INFO("BUTTON1 RELEASED");
+		}
+		else if(msg->state == kobuki_msgs::ButtonEvent::PRESSED){
+			ROS_INFO("BUTTON1 PRESSED");
+		}
+	}
+	else if(msg->state == kobuki_msgs::ButtonEvent::Button2){
+		if(msg->state == kobuki_msgs::ButtonEvent::RELEASED){
+			ROS_INFO("BUTTON2 RELEASED");
+		}
+		else if(msg->state == kobuki_msgs::ButtonEvent::PRESSED){
+			ROS_INFO("BUTTON2 PRESSED");
+		}
+	}
+}
+
+void cliffCallback(const kobuki_msgs::CliffEventConstPtr msg){
+  	if(msg->state == kobuki_msgs::CliffEvent::LEFT){
+		if(msg->state == kobuki_msgs::CliffEvent::FLOOR){
+			ROS_INFO("SENSOR LEFT FLOOR");
+		}
+		else if(msg->state == kobuki_msgs::CliffEvent::CLIFF){
+			ROS_INFO("SENSOR LEFT CLIFF");
+		}
+	}
+	else if(msg->state == kobuki_msgs::CliffEvent::CENTER){
+		if(msg->state == kobuki_msgs::CliffEvent::FLOOR){
+			ROS_INFO("SENSOR CENTER FLOOR");
+		}
+		else if(msg->state == kobuki_msgs::CliffEvent::CLIFF){
+			ROS_INFO("SENSOR CENTER CLIFF");
+		}
+	}
+	else if(msg->state == kobuki_msgs::CliffEvent::RIGHT){
+		if(msg->state == kobuki_msgs::CliffEvent::FLOOR){
+			ROS_INFO("SENSOR RIGHT FLOOR");
+		}
+		else if(msg->state == kobuki_msgs::CliffEvent::CLIFF){
+			ROS_INFO("SENSOR RIGHT CLIFF");
+		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -29,11 +95,14 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub_bumper = n.subscribe("/mobile_base/events/bumper", 1000, chatterCallback);
+  ros::Subscriber sub_bumper = n.subscribe("/mobile_base/events/bumper", 1000, bumperCallback);
 
-  ros::Subscriber sub_cliff = n.subscriber("mobile_base/events/cliff", 1000, chatterCallback);
+  ros::Subscriber sub_button = n.subscribe("/mobile_base/events/button", 1000, buttonCallback);
+
+  ros::Subscriber sub_cliff = n.subscribe("/mobile_base/events/cliff", 1000, cliffCallback);
 
   ros::spin();
 
   return 0;
 }
+
