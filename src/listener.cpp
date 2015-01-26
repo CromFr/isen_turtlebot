2163c4cb6b001b17718f21ad261cd3a23721e9b1
Gaetan <gaetan.lamour@isen-bretagne.fr>
2015-01-26 16:15:38 +0100

    Update listener.cpp CliffEvent

--- src/listener.cpp
+++ src/listener.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include <kobuki_msgs/BumperEvent.h>
+#include <kobuki_msgs/CliffEvent.h>
 
 void chatterCallback(const kobuki_msgs::BumperEventConstPtr msg)
 {
@@ -13,6 +14,13 @@ void chatterCallback(const kobuki_msgs::BumperEventConstPtr msg)
   else{
 	ROS_INFO("RELEASED");
   }
+
+  if(msg->state == kobuki_msgs::CliffEventConstPtr msg){
+	ROS_INFO("CLIFF");
+  }
+  else{
+	ROS_INFO("FLOOR");
+  }
 }
 
 int main(int argc, char **argv)
@@ -21,8 +29,11 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub = n.subscribe("/mobile_base/events/bumper", 1000, chatterCallback);
+  ros::Subscriber sub_bumper = n.subscribe("/mobile_base/events/bumper", 1000, chatterCallback);
+
+  ros::Subscriber sub_cliff = n.subscriber("mobile_base/events/cliff", 1000, chatterCallback);
 
   ros::spin();
+
   return 0;
 }
