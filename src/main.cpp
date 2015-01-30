0aa5b1bf5aa36e9033a8f3ae623994c525e8eb0f
viki <viki@r2d2.(none)>
2015-01-30 11:34:58 +0100

    Ajout code tracking couleur

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,10 @@ using namespace std;
 
 class Controller;
 Controller* ctrl;
+bool setColor = false;
+bool hasBeenSetColor = false;
+void mesureColorAverage(cv_bridge::CvImagePtr& cv_ptr,vector<int>& color);
+void giveTheObjectPosition(cv_bridge::CvImagePtr& cv_ptr, std::vector<int>& color, std::vector<int>& position, int sections, float threshold);
 
 ros::NodeHandle* node;
 ros::Subscriber 
@@ -54,7 +58,7 @@ public:
 			SetSpeed(-0.1, 0);
 		}
 
-		cout<<targetVisible<<"=> "<<target<<endl;
+		//cout<<targetVisible<<"=> "<<target<<endl;
 	}
 
 
@@ -137,7 +141,7 @@ void buttonCallback(const kobuki_msgs::ButtonEventConstPtr msg){
 	}
 	else if(msg->button == kobuki_msgs::ButtonEvent::Button2){
 		if(msg->state == kobuki_msgs::ButtonEvent::PRESSED)
-			ctrl->Play("~/hey.ogg");
+			setColor = true;
 	}
 }
 
@@ -159,6 +163,11 @@ void imgCallback(const sensor_msgs::ImageConstPtr msg){
 	i = (i+1)%3;
 	if(i!=0)return;
 
+	std::vector<int> color;
+	std::vector<int> position;//position in pixel of the object
+	int sections = 160;//we divide the picture in 160 parts
+	float threshold = 0.065;//detection threshold in % of 255, ex : 0.05 => 0.05*255
+
 	cv_bridge::CvImagePtr cv_ptr;
 	try{
 		cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
@@ -171,19 +180,157 @@ void imgCallback(const sensor_msgs::ImageConstPtr msg){
 	//===================
 	using namespace cv;
 
-	vector<Vec3f> circles;
-	HoughCircles(cv_ptr->image, circles, CV_HOUGH_GRADIENT, 1, cv_ptr->image.rows/4, 70, 100, 5, 0 );
-	if(circles.size()>0){
-		ctrl->targetVisible = true;
+	if(!hasBeenSetColor){
+		//we define and draw the rect
+		cv::rectangle(cv_ptr->image,cvPoint(290,210),cvPoint(350,270),CV_RGB(0,255,0),5,8);
 
-		ctrl->target = 2.0*(float)(circles[0][0])/(float)(cv_ptr->image.rows)-1.0;
+		if (setColor)
+		{
+			mesureColorAverage(cv_ptr, color);
+			setColor = false;
+			hasBeenSetColor = true;
+		}
+	}else{
+		cout << "coucou" << endl;
+		giveTheObjectPosition(cv_ptr, color, position, sections, threshold);
+		cout << "coucou2" << endl;
+		cv::rectangle(cv_ptr->image,cvPoint(position[0]-50,position[1]-50),cvPoint(position[0]+50,position[1]+50),CV_RGB(0,255,0),5,8);
+		cout << "coucou3" << endl;
 	}
-	else{
-		ctrl->targetVisible = false;
-	}
-
 	cv::imshow("img", cv_ptr->image);
 	cv::waitKey(3);
+}
+
+void mesureColorAverage(cv_bridge::CvImagePtr& cv_ptr,vector<int>& color)
+{
+	//we define an array which contain the average value of rgb colors of the rectangle
+	int averageColor[3]={0,0,0};
+	int count=0;
+
+	for(int x=298; x<=342;x++){
+		for(int y=218;y<=268;y++){
+			averageColor[0] += cv_ptr->image.at<cv::Vec3b>(y,x)[0];
+			averageColor[1] += cv_ptr->image.at<cv::Vec3b>(y,x)[1];
+			averageColor[2] += cv_ptr->image.at<cv::Vec3b>(y,x)[2];
+			count++;
+		}
+	}
+	for(size_t i=0; i <3; i++){
+		color.push_back((int)((double)averageColor[i]/(double)count));
+	}
+	cout << "Red : " << color[2] << " - Green : " << color[1] << " - Blue : " << color[0] << endl;
+}
+
+void giveTheObjectPosition(cv_bridge::CvImagePtr& cv_ptr, std::vector<int>& color, std::vector<int>& position, int sections, float threshold)
+{	
+	int pixelPerSectionsX = 640/sections;
+	int pixelPerSectionsY = 480/sections;
+	int startX = 0;
+	int endX = 0;
+	int startY = 0;
+	int endY = 0;
+	int averageColor[3] = {0,0,0};
+	int count = 0;
+	int testR = 0;
+	int testG = 0;
+	int testB = 0;
+	bool isColor[sections][sections];
+	int countColor = 0;
+	int countMax = 0;
+
+	cout << "hello" << endl;
+	position.clear();   // clear last position
+	
+	cout << "hello2" << endl;
+	for (int i=0; i<sections; i++){
+		for (int j=0; j<sections; j++){
+			isColor[i][j] = 0;
+		}
+	}
+	
+	cout << "hello3" << endl;
+	for(int boxY=0; boxY<sections; boxY++)
+	{
+		startY = pixelPerSectionsY * boxY;
+		endY = pixelPerSectionsY * (boxY+1); 
+		//we start to analyse the x orientation
+		for(int boxX=0; boxX<sections; boxX++)
+		{
+			testR = 0; testG = 0; testB = 0;
+			count = 0;
+			startX = pixelPerSectionsX * boxX;
+			endX = pixelPerSectionsX * (boxX+1); 
+			for(int x=startX; x<endX; x++)
+			{
+				for(int y=startY; y<endY; y++)
+				{
+					averageColor[0] += cv_ptr->image.at<cv::Vec3b>(y,x)[0];
+					averageColor[1] += cv_ptr->image.at<cv::Vec3b>(y,x)[1];
+					averageColor[2] += cv_ptr->image.at<cv::Vec3b>(y,x)[2];
+					count++;
+				}
+			}
+	cout << "hello4" << endl;
+			//now we calcul the average and we compare it with the threshold
+			averageColor[0] = averageColor[0]/count;
+			averageColor[1] = averageColor[1]/count;
+			averageColor[2] = averageColor[2]/count;
+			
+			if(averageColor[2] <= (color[2]+(int)(threshold*255)) && averageColor[2] >= (color[2]-(int)(threshold*255)))
+				testR = 1;
+			if(averageColor[1] <= (color[1]+(int)(threshold*255)) && averageColor[1] >= (color[1]-(int)(threshold*255)))
+				testG = 1;
+			if(averageColor[0] <= (color[0]+(int)(threshold*255)) && averageColor[0] >= (color[0]-(int)(threshold*255)))
+				testB = 1;
+			if(testR && testG && testB)//the color of this box correspond to the object
+			{
+				isColor[boxX][boxY] = 1;
+			}
+				
+		}
+		//we search for the maximums of the x and y sections in the tab isColor
+		
+	cout << "hello5" << endl;
+		for(int x=0; x<sections; x++)
+		{
+			for(int y=0; y<sections; y++)
+			{
+				if(isColor[x][y])
+				{
+					countColor++;
+				}
+			}
+			if(countColor>countMax)//we push and convert the position from sections to pixels
+			{
+				position.push_back(x*pixelPerSectionsX);
+				//printf("push x : %d ", x*pixelPerSectionsX);
+				countMax = countColor;
+			}
+			countColor = 0;
+
+		}
+	cout << "hello6" << endl;
+		countMax = 0;
+		for(int y=0; y<sections; y++)
+		{
+			for(int x=0; x<sections; x++)
+			{
+				if(isColor[x][y])
+				{
+					countColor++;
+				}
+			}
+			if(countColor>countMax)//we push and convert the position from sections to pixels
+			{
+				position.push_back(y*pixelPerSectionsY);
+				//printf("push y : %d ",y*pixelPerSectionsY);
+				countMax = countColor;
+			}
+			countColor = 0;
+		}
+		
+	}
+
 }
 
 
